ef177054996312e8c0253d17a89f07b0196be68b
dushaniw <dushaniwellappili@gmail.com>
2017-07-05 22:37:30 +0530

    add 1st attempt on regression

---
+++ Algorithms/Regression/LinearRegression/1stAttempt.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 21:34:21 2017
+
+@author: dushaniw
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+train = pd.read_csv('../train.csv')
+test = pd.read_csv('../test.csv')
+print ("Train data shape:", train.shape)
+print ("Test data shape:", test.shape)
+train.head()
+plt.style.use(style='ggplot')
+plt.rcParams['figure.figsize'] = (10, 6)
+train.SalePrice.describe()
+print ("Skew is:", train.SalePrice.skew())
+plt.hist(train.SalePrice, color='blue')
+plt.show()
+target = np.log(train.SalePrice)
+print ("Skew is:", target.skew())
+plt.hist(target, color='blue')
+plt.show()
+numeric_features = train.select_dtypes(include=[np.number])
+print numeric_features.dtypes
+corr = numeric_features.corr()
+print (corr['SalePrice'].sort_values(ascending=False)[:5], '\n')
+print (corr['SalePrice'].sort_values(ascending=False)[-5:])
+train.OverallQual.unique()
+quality_pivot = train.pivot_table(index='OverallQual',
+                                  values='SalePrice', aggfunc=np.median)
+print quality_pivot
+quality_pivot.plot(kind='bar', color='blue')
+plt.xlabel('Overall Quality')
+plt.ylabel('Median Sale Price')
+plt.xticks(rotation=0)
+plt.show()
+plt.scatter(x=train['GrLivArea'], y=target)
+plt.ylabel('Sale Price')
+plt.xlabel('Above grade (ground) living area square feet')
+plt.show()
+plt.scatter(x=train['GarageArea'], y=target)
+plt.ylabel('Sale Price')
+plt.xlabel('Garage Area')
+plt.show()
+train = train[train['GarageArea'] < 1200]
+plt.scatter(x=train['GarageArea'], y=np.log(train.SalePrice))
+plt.xlim(-200,1600) # This forces the same scale as before
+plt.ylabel('Sale Price')
+plt.xlabel('Garage Area')
+plt.show()
+nulls = pd.DataFrame(train.isnull().sum().sort_values(ascending=False)[:25])
+nulls.columns = ['Null Count']
+nulls.index.name = 'Feature'
+print nulls
+print ("Unique values are:", train.MiscFeature.unique())
+categoricals = train.select_dtypes(exclude=[np.number])
+print categoricals.describe()
+print ("Original: \n") 
+print (train.Street.value_counts(), "\n")
+train['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
+test['enc_street'] = pd.get_dummies(train.Street, drop_first=True)
+print ('Encoded: \n') 
+print (train.enc_street.value_counts())
+condition_pivot = train.pivot_table(index='SaleCondition',
+                                    values='SalePrice', aggfunc=np.median)
+condition_pivot.plot(kind='bar', color='blue')
+plt.xlabel('Sale Condition')
+plt.ylabel('Median Sale Price')
+plt.xticks(rotation=0)
+plt.show()
+def encode(x): return 1 if x == 'Partial' else 0
+train['enc_condition'] = train.SaleCondition.apply(encode)
+test['enc_condition'] = test.SaleCondition.apply(encode)
+condition_pivot = train.pivot_table(index='enc_condition', values='SalePrice', aggfunc=np.median)
+condition_pivot.plot(kind='bar', color='blue')
+plt.xlabel('Encoded Sale Condition')
+plt.ylabel('Median Sale Price')
+plt.xticks(rotation=0)
+plt.show()
+data = train.select_dtypes(include=[np.number]).interpolate().dropna() 
+y = np.log(train.SalePrice)
+X = data.drop(['SalePrice', 'Id'], axis=1)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+                                    X, y, random_state=42, test_size=.33)
+
+from sklearn import linear_model
+lr = linear_model.LinearRegression()
+
+model = lr.fit(X_train, y_train)
+print ("R^2 is: \n", model.score(X_test, y_test))
+predictions = model.predict(X_test)
+from sklearn.metrics import mean_squared_error
+print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+actual_values = y_test
+plt.scatter(predictions, actual_values, alpha=.75,
+            color='b') #alpha helps to show overlapping data
+plt.xlabel('Predicted Price')
+plt.ylabel('Actual Price')
+plt.title('Linear Regression Model')
+plt.show()
+
+submission = pd.DataFrame()
+submission['Id'] = test.Id
+feats = test.select_dtypes(
+        include=[np.number]).drop(['Id'], axis=1).interpolate()
+predictions = model.predict(feats)
+final_predictions = np.exp(predictions)
+submission['SalePrice'] = final_predictions
+submission.to_csv('../submissions/simple_regression.csv', index=False)
