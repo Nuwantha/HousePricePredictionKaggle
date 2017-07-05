8278cf9cf053e4fa773d647f254bc1e620f80c97
Nuwantha <nuwanthadew@gmail.com>
2017-07-05 17:40:32 +0530

    gradientboosting is added

---
+++ Algorithms/Gradient Boosting/base_model.py
@@ -0,0 +1,85 @@
+import datetime
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import ShuffleSplit
+from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from scipy.stats import skew
+ 
+def mean_squared_error_(ground_truth, predictions):
+    return mean_squared_error(ground_truth, predictions) ** 0.5
+RMSE = make_scorer(mean_squared_error_, greater_is_better=False)    
+    
+def create_submission(prediction,score):
+    now = datetime.datetime.now()
+    sub_file = 'submission_'+str(score)+'_'+str(now.strftime("%Y-%m-%d-%H-%M"))+'.csv'
+    print ('Creating submission: ', sub_file)
+    pd.DataFrame({'Id': test['Id'].values, 'SalePrice': prediction}).to_csv(sub_file, index=False)
+
+def data_preprocess(train,test):
+    outlier_idx = [4,11,13,20,46,66,70,167,178,185,199, 224,261, 309,313,318, 349,412,423,440,454,477,478, 523,540, 581,588,595,654,688, 691, 774, 798, 875, 898,926,970,987,1027,1109, 1169,1182,1239, 1256,1298,1324,1353,1359,1405,1442,1447]
+    train.drop(train.index[outlier_idx],inplace=True)
+    all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'],
+                          test.loc[:,'MSSubClass':'SaleCondition']))
+    
+    to_delete = ['Alley','FireplaceQu','PoolQC','Fence','MiscFeature']
+    all_data = all_data.drop(to_delete,axis=1)
+
+    train["SalePrice"] = np.log1p(train["SalePrice"])
+    #log transform skewed numeric features
+    numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+    skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
+    skewed_feats = skewed_feats[skewed_feats > 0.75]
+    skewed_feats = skewed_feats.index
+    all_data[skewed_feats] = np.log1p(all_data[skewed_feats])
+    all_data = pd.get_dummies(all_data)
+    all_data = all_data.fillna(all_data.mean())
+    X_train = all_data[:train.shape[0]]
+    X_test = all_data[train.shape[0]:]
+    y = train.SalePrice
+
+    return X_train,X_test,y
+    
+
+def model_gradient_boosting_tree(Xtrain,Xtest,ytrain):
+    
+    X_train = Xtrain
+    y_train = ytrain 
+    gbr = GradientBoostingRegressor(random_state=0)
+    param_grid = {
+        'n_estimators': [1500],
+ #       'max_features': [10,15],
+#	'max_depth': [6,8,10],
+        'learning_rate': [0.5],
+       'subsample': [0.8]
+    }
+    model = GridSearchCV(estimator=gbr, param_grid=param_grid, n_jobs=1, cv=10, scoring=RMSE)
+    model.fit(X_train, y_train)
+    print('Gradient boosted tree regression...')
+    print('Best Params:')
+    print(model.best_params_)
+    print('Best CV Score:')
+    print(-model.best_score_)
+
+    y_pred = model.predict(Xtest)
+    return y_pred, -model.best_score_
+
+
+# read data, build model and do prediction
+train = pd.read_csv("../../input/train.csv") # read train data
+test = pd.read_csv("../../input/test.csv") # read test data
+Xtrain,Xtest,ytrain = data_preprocess(train,test)
+
+
+test_predict,score = model_gradient_boosting_tree(Xtrain,Xtest,ytrain)
+
+create_submission(np.exp(test_predict),score)
+
+
+
